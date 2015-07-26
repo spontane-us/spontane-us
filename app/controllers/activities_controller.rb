ca46e18555df2850a450e106d4778b03053eecde
whitney <whitneywooyy@gmail.com>
2015-07-25 21:56:21 -0600

    Completed story2

---
+++ app/controllers/activities_controller.rb
@@ -0,0 +1,66 @@
+class ActivitiesController < ApplicationController
+  before_action :set_activity, only: [:show, :edit, :update, :destroy]
+  # before_action :authenticate_user!, except: :index
+
+  def search
+  end 
+
+  def index
+    @activities = Activity.all
+  end
+
+  def show
+  end
+
+  def new
+    @activity = Activity.new
+  end
+
+  def edit
+  end
+
+  def create
+    @activity = Activity.new(activity_params)
+
+    respond_to do |format|
+      if @activity.save
+        format.html { redirect_to @activity, notice: 'Activity was successfully created.' }
+        format.json { render :show, status: :created, location: @activity }
+      else
+        format.html { render :new }
+        format.json { render json: @activity.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @activity.update(activity_params)
+        format.html { redirect_to @activity, notice: 'Activity was successfully updated.' }
+        format.json { render :show, status: :ok, location: @activity }
+      else
+        format.html { render :edit }
+        format.json { render json: @activity.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @activity.destroy
+    respond_to do |format|
+      format.html { redirect_to activities_url, notice: 'Activity was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_activity
+      @activity = Activity.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def activity_params
+      params.require(:activity).permit(:title, :description, :max_people, :no_max, :zipcode, :start_time, :end_time)
+    end
+end
